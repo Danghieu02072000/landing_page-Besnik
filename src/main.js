e2898433b528c2d418039d42b24cd8b297000565
Dang Hieu <danghieutieumai@gmail.com>
2023-09-30 01:48:24 +0700

    done add animation

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ const LOGO = document.querySelector('#logo');
 const MENU_MOBIE = document.querySelector('#menu_mobie')
 const CLOSE = document.querySelector('#close');
 const OVERLAY = document.querySelector('#overlay');
-
+AOS.init({
+    duration: 300,
+  })
 LOGO.onclick = function () {
     MENU_MOBIE.classList.toggle('show')
     OVERLAY.classList.toggle('show1')
@@ -33,5 +35,4 @@ $('.owl-carousel').owlCarousel({
             items:4
         }
     }
-})
-  
+});
