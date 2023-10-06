b745ccdc08dc00f106c2276808e8f941674cb887
Dang Hieu <danghieutieumai@gmail.com>
2023-10-06 21:10:59 +0700

    config postcss with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
     screens: {
       '3xs': '330px',
